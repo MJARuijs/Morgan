832ff2a355660221e4e0fc0767d18ed102777c4e
MJARuijs <m.j.a.ruijs@student.tudelft.nl>
2020-02-07 17:18:03 +0100

    Setup gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.gradle.internal.os.OperatingSystem
+
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "com.blazit"
+version = "1.0"
+val lwjglVersion = "3.2.3"
+
+val lwjglNatives = when (OperatingSystem.current()) {
+    OperatingSystem.LINUX -> "natives-linux"
+    OperatingSystem.MAC_OS -> "natives-macos"
+    OperatingSystem.WINDOWS -> "natives-windows"
+    else -> throw Error("Unrecognized or unsupported Operating system. Please set \"lwjglNatives\" manually")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+}
+
+tasks {
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
